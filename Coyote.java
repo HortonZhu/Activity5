56c22c45505be4b28d98ce5e1f3e7cfd6de3095d
HortonZhu <hortonhearsazhu@gmail.com>
2025-04-02 12:46:56 -0700

    Add files via upload

--- Coyote.java
+++ Coyote.java
@@ -7,8 +7,12 @@ import java.util.ArrayList;
 
 public class Coyote extends Critter
 {
+	private int consecutiveSteps;
+	private int waitCount;
 	public Coyote()
 	{
+		consecutiveSteps = 0;
+		waitCount = 0;
 		setColor(null);
 		ArrayList<Integer> directions = new ArrayList<Integer>();
 		for (int i = 0; i < 8; i++)
@@ -33,11 +37,50 @@ public class Coyote extends Critter
 	}
 	public void makeMove(Location loc)
 	{
-		//if blocked in front
+		//if blocked
 		if ( loc.equals(getLocation()) )
 		{
+			//blocked by edge of grid
+			if ( !getGrid().isValid(loc.getAdjacentLocation(getDirection())) )
+			{
+				//wait 5 
+				waitCount++;
+				if ( waitCount > 5 )
+				{
+					//choose random new direction (must be empty and valid)
+					waitCount = 0;
+					setDirection( getDirectionToward( getGrid().getEmptyAdjacentLocations(loc)
+						[Math.random() * getGrid().getEmptyAdjacentLocations(loc).size()] ));
+				}
+			}
+			//blocked by actor
+			else
+			{
+				//blocked by boulder
+				if ( getGrid().get(loc.getAdjacentLocation(getDirection())) instanceof Boulder )
+				{
+					Kaboom kaboom = new Kaboom();
+					kaboom.putSelfInGrid( loc.getAdjacentLocation(getDirection()) );
+					removeSelfFromGrid();
+					return;
+				} 
+				else
+				{
+				waitCount++;
+					if ( waitCount > 5 )
+					{
+						//INCOMPLETE ADD ROCK
+						waitCount = 0;
+						setDirection( getDirectionToward( getGrid().getEmptyAdjacentLocations(loc)
+							[Math.random() * getGrid().getEmptyAdjacentLocations(loc).size()] ));
+					}
+				}
+			}
+			//blocked by another actor
+			else
+			{
+				
+			}
 		}
-		if ( getGrid().isValid(loc) );
-			moveTo(loc);
 	}
 }
